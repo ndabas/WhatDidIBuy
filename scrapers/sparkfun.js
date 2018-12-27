58d9ce53ce7a0538b8c972c3e2f08c8a3046f5b8
Nikhil Dabas <nikhil@nikhildabas.com>
2018-12-27 17:31:08 +0530

    Add SparkFun

---
+++ scrapers/sparkfun.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const EventEmitter = require('events');
+const parse = require('csv-parse/lib/sync')
+const utils = require('../utils');
+
+module.exports = exports = new EventEmitter();
+
+/**
+ * @param browser { import("puppeteer").Browser }
+ */
+exports.scrape = async function (browser) {
+  const page = await browser.newPage();
+
+  // The CAPTCHA on the login page seems to be easier to pass with the following line
+  await utils.hideWebDriver(page);
+  await page.goto('https://www.sparkfun.com/');
+
+  // Wait for the user to log in and go to the order history page
+  await page.waitForSelector('a[href="https://www.sparkfun.com/orders.json"]', { timeout: 0 });
+
+  // Download the CSV data files
+  const downloadText = url => window.fetch(url).then(resp => resp.text());
+
+  const orders = JSON.parse(await page.evaluate(downloadText, 'https://www.sparkfun.com/orders.json'));
+  console.log(`Found ${orders.length} orders.`);
+
+  for (const order of orders) {
+    this.emit('order', {
+      id: order.id,
+      date: order.date,
+      status: order.status
+    });
+
+    const productsCsv = await page.evaluate(downloadText, `https://www.sparkfun.com/orders/${order.id}.csv`);
+    const items = parse(productsCsv, { columns: true });
+    let idx = 1;
+    for (const item of items) {
+      this.emit('item', {
+        ord: order.id,
+        dpn: item.sku,
+        idx: idx++,
+        qty: item.quantity,
+        dsc: item.name,
+        upr: item['unit price (USD)']
+      });
+    }
+  }
+};
